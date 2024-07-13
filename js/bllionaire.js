698b4492e4c91721d95c6b3e0732cf53d342057d
Rubel Rudra <rubelrudra27@gmail.com>
2024-07-13 18:46:50 +0600

    fetch data and load all person

--- js/bllionaire.js
+++ js/bllionaire.js
@@ -3,8 +3,34 @@ document.getElementById('menu-button').addEventListener('click', function() {
   const title = document.getElementById('title');
   if (sidebar.classList.contains('hidden')) {
       sidebar.classList.remove('hidden');
-      title.classList.add('hidden');
   } else {
       sidebar.classList.add('hidden');
   }
 });
+
+const loadPerson=async()=>{
+  try{
+    const res=await fetch('https://forbes400.onrender.com/api/forbes400?limit=10');
+  const data=await res.json();
+  displayPerson(data);
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+
+document.getElementById('all-person').addEventListener('click', function(){
+  loadPerson();
+})
+
+const displayPerson=(person)=>{
+  console.log(person);
+  const personContainer=document.getElementById('person-container');
+  personContainer.innerHTML='';
+  person.forEach(person=>{
+    console.log(person);
+    const div=document.createElement('div');
+    div.classList.add('person');
+    div.innerHTML=``
+  })
+}
